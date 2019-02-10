088a7a3aecb7f63c40dcee8172159fafb1b0e140
Jaideep Singh <SinghJaideepJi@gmail.com>
2019-02-10 05:17:06 -0500

    Implemented HashRate Difficulty Transactions API calls and calculations

--- main.js
+++ main.js
@@ -1,25 +1,48 @@
-apiKey = "&apikey=2957NRM1W9NEHAVICBVTWCG4PNE1RDCD3V";
-apiBlockNum = "https://api.etherscan.io/api?module=block&action=getblockreward&blockno=2165403"+ apiKey;
-apiLastBlockNum = "https://api.etherscan.io/api?module=proxy&action=eth_blockNumber"+ apiKey;
-apiPrice = "https://api.etherscan.io/api?module=stats&action=ethprice"+ apiKey;
-apiSupply = "https://api.etherscan.io/api?module=stats&action=ethsupply"+ apiKey;
+var apiKey = "&apikey=2957NRM1W9NEHAVICBVTWCG4PNE1RDCD3V";
+var apiBlockNum = "https://api.etherscan.io/api?module=block&action=getblockreward&blockno=2165403"+ apiKey;
+var apiLastBlockNum = "https://api.etherscan.io/api?module=proxy&action=eth_blockNumber"+ apiKey;
+var apiPrice = "https://api.etherscan.io/api?module=stats&action=ethprice"+ apiKey;
+var apiSupply = "https://api.etherscan.io/api?module=stats&action=ethsupply"+ apiKey;
+var apiAddress = "";
+var apiBlock = "";
+var apiTransactions = "";
 
 // Global varibale to use between data updations
 var prevPriceUsd = 0;
 var addressInput = $("input[name=address]");
 var priceUsd = 0;
+var blockNumberHex = 0;
 
-// Fetch te Last Block and Display it
+// Execute the script after page loading is complete
+window.onload = function(){
+updateData();
+// Add event handler to Address Input
+addressInput = $("input[name=address]");
+addressInput.change(() => fetchAddress());
+}
+
+// Update the page data every few seconds
+//setInterval(updateData, 3000);
+
+const updateData = function(){
+  console.log("Updating Data");
+  fetchLastBlockNum();
+  fetchPrice();
+}
+
+// Fetch the Last Block and Display it
 async function fetchLastBlockNum(){
     const response_LastBlockNum = await fetch(apiLastBlockNum);
     const json_LastBlockNum = await response_LastBlockNum.json();
-    // console.log(json_LastBlockNum);
-    var blockNumber = parseInt(json_LastBlockNum.result,16);
-    console.log("Latest Block nuber - "+blockNumber);
+    //console.log(json_LastBlockNum);
+    blockNumberHex = json_LastBlockNum.result;
+    var blockNumber = parseInt(blockNumberHex,16);
+    console.log("Latest Block number - "+blockNumber);
     $("#lastBlock").text(blockNumber);
+    fetchDifficultyHash();
 }
 
-// Fetch lastes Price and Market Cap of Ether
+// Fetch lastest Price and Market Cap of Ether
 async function fetchPrice(){
     //Fetch Price
     const response_Price = await fetch(apiPrice);
@@ -50,6 +73,27 @@ async function fetchPrice(){
     $("#marketCap").text(`MARKET CAP OF $${marketCap.toPrecision(5)} BILLION`);
 }
 
+// Fetch the Last Block and Calculate Hash Rate, Network Difficulty and Number of Transactions
+async function fetchDifficultyHash(){
+  console.log("BlockNumberHex - ",blockNumberHex);
+  apiBlock = "https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag="+ blockNumberHex +"&boolean=true"+ apiKey;
+  try{
+    const response_Block = await fetch(apiBlock);
+    const json_Block = await response_Block.json();
+    // console.log(json_Block);
+    var difficulty = parseInt(json_Block.result.difficulty,16)/10**12;
+    console.log("Difficulty - "+difficulty);
+    $("#networkDifficulty").text(difficulty.toFixed(2)+" TH");
+    var hashRate = difficulty / 15 * 1000;
+    $("#hashRate").text(hashRate.toFixed(2)+" GH/s");
+    var transactionNum = json_Block.result.transactions.length;
+    // console.log(transactionNum);
+    $("#transactions").text(transactionNum);
+  }catch{
+    // Try next time
+  }
+}
+
 // display ALL Transactions related to the Entered Address
 async function fetchAddress(){
   addressValue = addressInput.val();
@@ -73,20 +117,3 @@ async function fetchAddress(){
     $("#Trax").text(" ");
   }
 }
-
-const updateData = function(){
-    console.log("Updating Data");
-    fetchLastBlockNum();
-    fetchPrice();
-}
-
-// Execute the script after page loading is complete
-window.onload = function(){
-  updateData();
-// Add event handler to Address Input
-addressInput = $("input[name=address]");
-addressInput.change(() => fetchAddress());
-}
-
-// Update the page data every few seconds
-setInterval(updateData, 3000);
